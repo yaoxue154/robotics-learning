7a9be9937296b3a74671440a6fe33edf519a0ff8
yaoxue <15750205131@163.com>
2026-07-26 14:16:49 +0800

    python的基础理解和简单的操作

---
+++ stage01/code/hello.py
@@ -0,0 +1,6 @@
+name="yaoxue"
+major="机器人工程"
+goal="打比赛，进实验室"
+print(name)
+print(major)
+print(goal)
---
+++ stage01/code/vars.py
@@ -0,0 +1,7 @@
+name="yaoxue" 
+age=18
+score=95.5
+print(type(name))
+print(type(age))
+print(type(score))  
+print(name,age,score)
