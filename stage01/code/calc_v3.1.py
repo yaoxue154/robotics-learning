2d57a2faf90b95472828d53982f882bee63dd4be
yaoxue <15750205131@163.com>
2026-07-28 18:53:09 +0800

    程序修订

--- stage01/code/calc_v3.1.py
+++ stage01/code/calc_v3.1.py
@@ -1,12 +1,11 @@
 count=0
 while True:
-    first_number=float(input("the first number: "))
-    second_number=float(input("the second number: "))
     operation=input("the operation: ")
     if operation=="q": 
-        break
+            break
+    first_number=float(input("the first number: "))
+    second_number=float(input("the second number: "))
     count=count+1
-    print(f"service time:{count}")
     if second_number==0 and operation=="/":
         print("Error: Division by zero is not allowed.")
     elif operation=="+":
@@ -19,3 +18,4 @@ while True:
         print(f"{first_number} / {second_number} = {first_number / second_number}")
     else:
         print("Invalid operation")
+print(f"已服务{count}次")
