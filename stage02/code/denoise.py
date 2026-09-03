35c60045b166fc1552475d2c93bd2e457f8eaa8d
yaoxue <15750205131@163.com>
2026-09-04 00:18:19 +0800

    画图plot的一天，噪声里挖信号，用数学解决问题

---
+++ stage02/code/denoise.py
@@ -0,0 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+truth=2.0
+noise=np.random.normal(0,0.3,100)
+readings=truth+noise
+plt.plot(readings,'o')
+plt.axhline(y=truth,color='r',label='truth')
+plt.ylim(0,4)
+plt.legend()
+plt.show()
+
+estimate=np.mean(readings)
+print(readings[0])
+print(estimate)
+print(truth)
+print(readings[0]-truth)
+print(estimate-truth)  
+
+t=np.arange(0,10,0.1)
+truth=2+t*0.5
+readings=truth+noise
+fliters=[]
+for i in range(5,len(readings)-5):
+    windows=readings[i-5:i+6]
+    fliters.append(windows.mean())
+plt.plot(t[0:6],readings[0:6],'o',label='unseen')
+plt.plot(t[-5:],readings[-5:],'o',label='unseen')
+plt.plot(t[5:-5],fliters,'r',label='flitered')
+plt.plot(t,noise,'g',label='original_noise_data',marker='o')
+plt.plot(t,truth,'k',label='truth')
+plt.legend()
+plt.show()
+
+
+
+
